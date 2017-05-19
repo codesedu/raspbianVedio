aa88dc5200eb7b5affcb299c2e4e7e2cf058e9e6
uglyN0u <1215714557@qq.com>
2017-05-19 21:25:51 +0800

    Create videosetup.sh

---
+++ videosetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#@date 2017-05-12
+#@author qinfsoft
+sudo apt-get update
+sudo apt-get install subversion
+sudo apt-get install libjpeg8-dev
+sudo apt-get install imagemagick
+sudo apt-get install libv4l-dev
+sudo apt-get install cmake
+sudo apt-get install unzip
+git clone https://github.com/jacksonliam/mjpg-streamer.git
+unzip mjpg-streamer
+cd mjpg-streamer/mjpg-streamer-experimental
+make all
+sudo make install
+sudo chmod 777 start.sh
+./start.sh
